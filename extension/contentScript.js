c66c2ff449ebe9d4e8136a0e41c98fdb2ba54690
James Kerrane <hi@jameskerrane.com>
2020-07-13 02:05:44 -0600

    ✨ FEATURE: Add test page with sample images
    Adds a test page with sample images for the extension to test features with.

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,3 +1,7 @@
-console.log("altML is now running JS on this page!");
+console.info("altML: altML is now running JS on this page!");
+
+if(window.location.href.includes(chrome.runtime.getURL("test.html"))) { // If the URL matches the test page
+  console.warn("altML: altML might run a little differently on this page than on the rest of the internet. You have been warned!");
+}
 
 // Insert code that find empty alt tags in images, and replaces them with generated alt tags.
